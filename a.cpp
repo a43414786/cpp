88898c1525394cbb116cae0ea32b88c826ba3dd3
a43414786 <72087693+a43414786@users.noreply.github.com>
2021-08-17 17:12:15 +0800

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 #include<vector>
 #include<cstring>
+struct ListNode{
+    int val;
+    ListNode*next;
+    ListNode():
+};
+class Solution{
+public:
+    
+
+};
 int main(){
     return 0;
 }
